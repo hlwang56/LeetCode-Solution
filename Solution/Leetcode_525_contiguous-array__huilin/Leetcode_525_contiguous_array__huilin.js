533dc8eaa4ab8dc1dd0d415e2a60c8e318fccff5
hlwang56 <69446689+hlwang56@users.noreply.github.com>
2021-12-10 22:57:32 +0800

    Create Leetcode_525_contiguous_array__huilin.js

---
+++ Solution/Leetcode_525_contiguous-array__huilin/Leetcode_525_contiguous_array__huilin.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function(nums) {
+    let ans = new Map();
+    ans.set(0, -1);//initialize:(sum, index)
+    var sum = 0;
+    var l = 0;
+    for(var i = 0;i < nums.length;i++){
+        sum += nums[i] == 0? -1 : 1;
+        if(ans.has(sum)){
+            l = Math.max(l, i - ans.get(sum))
+        }else{
+            ans.set(sum, i);
+        }
+    }
+    return l;
+
+};
